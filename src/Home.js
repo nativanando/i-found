123c068b9eb0df5dca947eba0c87ed718d51003b
nativanando <nativanando@gmail.com>
2020-03-15 14:06:28 -0300

    adding navigatior transition

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, View, Text, DeviceEventEmitter } from 'react-native';
+import { StyleSheet, View, Text, DeviceEventEmitter, StatusBar, SafeAreaView } from 'react-native';
 import Beacons from 'react-native-beacons-manager';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 const home = () => {
 
@@ -30,14 +31,21 @@ const home = () => {
   });
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.btleConnectionStatus}>
-        Bluetooth connection status: {bluetoothState ? bluetoothState : 'NA'}
-      </Text>
-      <Text style={styles.headline}>
-        All beacons in the area
+    <>
+      <StatusBar backgroundColor="#84769a" barStyle="light-content" />
+      <SafeAreaView>
+        <View style={styles.body}>
+          <View style={styles.content}>
+            <Text style={styles.textColor}>
+              Bluetooth connetado: {bluetoothState ? bluetoothState : 'NA'}
+            </Text>
+            <Text style={styles.headline}>
+              Dispositivos próximos
         </Text>
-    </View>
+          </View>
+        </View>
+      </SafeAreaView>
+    </>
   );
 
 }
@@ -56,7 +64,8 @@ const styles = StyleSheet.create({
   },
   headline: {
     fontSize: 20,
-    paddingTop: 20
+    paddingTop: 20,
+    color: Colors.white
   },
   row: {
     padding: 8,
@@ -64,6 +73,57 @@ const styles = StyleSheet.create({
   },
   smallText: {
     fontSize: 11
+  },
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: '#563d7c',
+    height: '100%',
+    width: '100%',
+  },
+  content: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    textAlign: 'center',
+    fontSize: 20,
+    color: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
+  textColor: {
+    color: Colors.white,
   }
 });
 
--- src/Navigator.js
+++ src/Navigator.js
@@ -1,16 +1,30 @@
+import React from 'react';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import createAnimatedSwitchNavigator from 'react-navigation-animated-switch';
+import { Transition } from 'react-native-reanimated';
+
 import Greeting from './Greeting';
 import Home from './Home';
 
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
+const Navigator = createAnimatedSwitchNavigator({
+  Greeting: { screen: Greeting },
+  Home: { screen: Home }
+}, {
+  headerMode: 'none',
+  initialRouteName: 'Greeting',
+  resetOnBlur: false,
+  backBehavior: 'history',
+  transition: (
+    <Transition.Together>
+      <Transition.Out
+        type="scale"
+        durationMs={400}
+        interpolation="easeOut"
+      />
+      <Transition.In type="fade" durationMs={400} />
+    </Transition.Together>
+  ),
+});
 
-const Navigator = createAppContainer(
-    createStackNavigator({
-        Greeting: Greeting,
-        Home: Home,
-    }, {
-        headerMode: 'none'
-    })
-);
-
-export default Navigator;
+export default createAppContainer(Navigator);
